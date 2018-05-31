4d44c5abdc07cff5d968cf47b9b27caf7aa74916
kasambuli <cynthiakasambuli@gmail.com>
2018-05-31 12:05:33 +0300

    add bootstrap4

--- news/settings.py
+++ news/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'bootstrap4',
-    ''
+    'newsblog.apps.NewsblogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
