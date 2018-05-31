f867736779ec313d728299cf464c0d5a4fc4f8b3
kasambuli <cynthiakasambuli@gmail.com>
2018-05-31 12:12:42 +0300

    add index view

--- newsblog/views.py
+++ newsblog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+@login_required(login_url='/accounts/login/')
 def index(request):
     '''
     function to diplay all news articles
