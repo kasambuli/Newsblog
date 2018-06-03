a3ed14baae1aa8e24d0bced15ee64066c139670b
kasambuli <cynthiakasambuli@gmail.com>
2018-06-04 00:03:32 +0300

    add Comment and News urls

--- newsblog/urls.py
+++ newsblog/urls.py
@@ -5,4 +5,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'editprofile/(\d+)/$', views.edit_profile, name='edit_profile'),
+    url(r'news/',views.news, name = 'news'),
+    url(r'comment/',views.comment, name = 'comment'),
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
